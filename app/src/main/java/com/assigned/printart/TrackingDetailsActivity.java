8b745247a86e1876cc458c4678a092fa1b776b46
KimonnAravind <aravindselvam03@gmail.com>
2020-05-23 20:57:05 +0530

    git commit -m "PrintArt"

--- app/src/main/java/com/assigned/printart/TrackingDetailsActivity.java
+++ app/src/main/java/com/assigned/printart/TrackingDetailsActivity.java
@@ -3,13 +3,17 @@ package com.assigned.printart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TrackingDetailsActivity extends AppCompatActivity {
-
+    TextView orderedDate;
+    String ordereddate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking_details);
 
+        orderedDate = (TextView) findViewById(R.id.orderedDate);
+        ordereddate =
     }
 }
